6d2c8376bfb606b294552bbb9e292b82404bb0cb
pagre <92362794+pagre8@users.noreply.github.com>
2024-09-14 12:32:18 +0200

    Initial push

---
+++ API/Controllers/ChatsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatsController : ControllerBase
+    {
+
+        [HttpGet("chats/{id}/messages")]
+        public IActionResult GetMessages([FromQuery] string cursor, string id)
+        {
+            if (string.IsNullOrWhiteSpace(cursor)) {  return BadRequest("cursor parameter is required"); }
+            if (string.IsNullOrWhiteSpace(id)) { return BadRequest("id parameter is requiered"); }
+
+        }
+    }
+}
